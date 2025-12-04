f504d929d35ff8a176f32c220951da1deaa0e2f0
dias123ve <diassyahrian3004@gmail.com>
2025-12-04 18:42:25 +0700

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -5,7 +5,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { BookOpen, ArrowLeft } from "lucide-react";
 import { useToast } from "@/hooks/use-toast";
-import { supabase } from "@/supabaseClient";
+import { supabase } from "../lib/supabaseClient";
 
 const Login = () => {
   const navigate = useNavigate();
